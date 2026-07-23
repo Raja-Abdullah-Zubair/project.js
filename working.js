6bb4a1297fbb45de600b37eb5720bf7e63ff2d4d
Abdullah-Bin-Zubair <970rajaabdullah9089@gmail.com>
2026-07-23 13:26:51 +0500

    feat:new data

--- working.js
+++ working.js
@@ -1,12 +1,38 @@
-let studentName = [
+// let studentName = [
+//   {
+//     name: "Ali",
+//     class: "11",
+//     city: "wah cantt",
+//   },
+//   {
+//     name: "Ahmad",
+//     class: "12",
+//     city: "wah cantt",
+//   },
+//   {
+//     name: "husain",
+//     class: "11",
+//     city: "wah cant",
+//   },
+// ];
+
+const data = [
   {
-    name: "Ali",
-    class: "11",
-    city: "wah cantt",
+    id: 12,
   },
   {
-    name: "Ahmad",
-    class: "12",
-    city: "wah cantt",
+    id: 13,
+  },
+  {
+    id: 14,
+  },
+  {
+    id: 15,
   },
 ];
+test(data);
+function test(arr) {
+  for (let i = 0; i <= 3; i++) {
+    console.log(`value ${i + 1}:${arr[i].id}`);
+  }
+}
